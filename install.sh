0f7b9f6669a5f692a46d07339e1bc4cbe32ac66c
Paul Hunter <p.hunter.eng@gmail.com>
2015-03-18 16:35:20 -0700

    First commit,
    Making public so we can curl for it.

---
+++ install.sh
@@ -0,0 +1,102 @@
+
+#!/bin/bash
+
+# Day Trading - CSC 466
+# Installation Script
+# Usage
+# ./install <SystemType>
+# <SystemType> : 'tr' | 'lb' | 'ls' | 'qs'
+# 	tr - Transaction Server
+#	lb - Load Balancing Server
+#   ls - Log Server
+#   qs - Quote Server
+
+
+clear
+#Check that we have enough parameters
+if [[ $# < 2 ]]; then
+	echo "Too few parameters!"
+    echo "  Usage: ./install.sh <Bitbucket Username> <System-Type>"
+	exit
+else
+	if [[ $2 != "tr" && $2 != "lb" && $2 != "ls" && $2 != "qs" ]]; then
+		echo "Incorrect System Type!"
+        echo "Choices include:"
+        echo "    tr - Transaction Server"
+        echo "    lb - Load Balancer"
+        echo "    ls - Log Server"
+        echo "    qs - Quote Server"
+		exit
+	fi
+fi
+
+clear 
+echo "Cleaning Past Install"
+rm -rf /Users/${USER}/postgres
+rm -rf /Users/${USER}/day_trading
+
+echo "+ ----------------------------------------------------------------- +"
+echo "Fetching Source Codes"
+mkdir /Users/${USER}/day_trading
+#Exec it so if it fails, we stop the script.
+exec git clone "https://$1@bitbucket.org/romilkhanna/day-trading.git" /Users/${USER}/day_trading
+# User will be prompted for password.
+
+cd /Users/${USER}/Desktop
+mkdir dt_install
+cd dt_install
+echo "Fetching Postgres 9.4.1"
+curl -o postgresql-9.4.1.tar.gz https://ftp.postgresql.org/pub/source/v9.4.1/postgresql-9.4.1.tar.gz
+echo "Fetching Psycopg2"
+curl -o psycopg2 http://initd.org/psycopg/tarballs/PSYCOPG-2-6/psycopg2-2.6.tar.gz
+
+echo "Unpacking Sources..."
+gzip postgresql-9.4.1.tar.gz
+tar xf postgresql-9.4.1.tar
+gzip psycopg2-2.6.tar.gz
+tar xf psycopg2-2.6.tar
+
+echo "+ ----------------------------------------------------------------- +"
+echo "Installing Postgres 9.4.1"
+mkdir /Users/${USER}/postgres
+cd postgresql-9.4.1/
+./configure --prefix=/Users/${USER}/postgres/
+make install
+mkdir /Users/${USER}/postgres/db
+/Users/${USER}/postgres/bin/initdb -D /Users/${USER}/postgres/db
+cd ../
+
+echo "+ ----------------------------------------------------------------- +"
+echo "Installing Psycopg2 2.6"
+cd psycopg2-2.6 
+python setup.py build_ext --pg-config=/Users/${USER}/postgres/bin/pg_config 
+python setup.py install --user
+cd ../
+
+echo "+ ----------------------------------------------------------------- +"
+echo "Copying Default Network Configuration for Postgres"
+cd configs
+cp postgresql.conf /Users/${USER}/postgres/db/postgresql.conf
+cp pg_hba.conf /Users/${USER}/postgres/db/pg_hba.conf
+cd ../
+
+echo "+ ----------------------------------------------------------------- +"
+echo "Starting Postgres"
+/Users/${USER}/postgres/bin/pg_ctl start -D ../db
+
+echo "Creating Database Role"
+export PGPASSWORD=hunter2
+/Users/${USER}/postgres/bin/pg_ctl start -D /Users/${USER}/postgres/db
+/Users/${USER}/postgres/bin/psql postgres -h 127.0.0.1 -p 3000 -U test -f ./configs/createrole.sql
+
+
+echo "Cleaning up Desktop"
+cd ~/Desktop
+rm -rf dt_install
+
+echo "+ ----------------------------------------------------------------- +"
+echo "Install Complete"
+
+
+
+	
